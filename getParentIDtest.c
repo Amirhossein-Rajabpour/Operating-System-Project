5bfe0477a0afb2911150d417595da512ce2e2d62
amirhoseinrj <rajabpouramirhosein@gmail.com>
2021-02-02 23:52:59 +0330

    getParentIDtest completed

--- getParentIDtest.c
+++ getParentIDtest.c
@@ -6,9 +6,14 @@ int main()
 {
     for (int i = 0; i < 1; i++)
     {
-        fork();
-        printf(1, "This is process (getpid) %d and the parent id is %d\n", getpid(), getParentID());
-        // printf(1, "This is process (fork return) %d and the parent id is %d\n\n", pid, getParentID());
+        int pid = fork();
+        if (pid > 0){ // parent
+            sleep(100);
+            printf(1, "This is process (getpid) %d and the parent id is %d\n", getpid(), getParentID());
+            wait();
+        }
+        else // child
+            printf(1, "This is process (getpid) %d and the parent id is %d\n", getpid(), getParentID());
     }
     exit();
 }
