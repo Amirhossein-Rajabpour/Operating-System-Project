2c0e14de60f2efff941a80d355969b2ab14be562
Danial <Daniiiaal.ha@gmail.com>
2021-02-03 09:03:47 -0800

    remove redundant lines

--- getSyscallCounterTest.c
+++ getSyscallCounterTest.c
@@ -5,7 +5,6 @@
 int main(int argc, char *argv[])
 {
     int syscall_num = atoi(argv[1]);
-    // printf(1, "Syscall #%d has been called.\n", syscall_num);
     printf(1, "Syscall #%d has been called %d times\n", syscall_num, getSyscallCounter(syscall_num));
     exit();
 }
