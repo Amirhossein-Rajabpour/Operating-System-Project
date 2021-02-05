d20e31b5289385ee9b412cd3148b467348b27416
amirhoseinrj <rajabpouramirhosein@gmail.com>
2021-02-05 23:33:15 +0330

    roundRobinTest implemented (has bugs)

--- Makefile
+++ Makefile
@@ -184,6 +184,8 @@ UPROGS=\
 	_getParentIDtest\
 	_getChildrenTest\
 	_getSyscallCounterTest\
+	_roundRobinTest\
+
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -253,7 +255,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 EXTRA=\
 	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
-	getParentIDtest.c getChildrenTest.c getSyscallCounterTest.c\
+	getParentIDtest.c getChildrenTest.c getSyscallCounterTest.c roundRobinTest.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
--- roundRobinTest.c
+++ roundRobinTest.c
@@ -5,13 +5,24 @@
 int main(int argc, char *argv[])
 {
     int main_pid = getpid();
-    for (int i = 0 ; i < 4 ; i++)
-        fork();
+    for (int i = 0 ; i < 10 ; i++)
+    {
+        if ( fork() == 0 )
+            break;
+    }
 
     if (main_pid != getpid())
     {
         for (int i = 0 ; i < 1000 ; i++)
             printf(1, "/%d/: %d\n", getpid(), i);  
+        
+        int turnAroundTime = getTurnAroundTime(getpid());
+        int waitingTime    = getWaitingTime(getpid());
+        int CBT            = getCBT(getpid());
+
+        printf(1, "turnAroundTime for process /%d/ is :%d\n", getpid(), turnAroundTime);  
+        printf(1, "waitingTime for process /%d/ is :%d\n", getpid(), waitingTime);  
+        printf(1, "CBT for process /%d/ is :%d\n", getpid(), CBT);  
     }
 
     while (wait() != -1){;}
