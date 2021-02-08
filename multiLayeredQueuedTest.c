57fe8fdbfb9373bf10f1986885297b1307be8c97
amirhoseinrj <rajabpouramirhosein@gmail.com>
2021-02-08 13:13:10 +0330

    multilayered modified

--- multiLayeredQueuedTest.c
+++ multiLayeredQueuedTest.c
@@ -5,15 +5,39 @@
 // number of children created
 #define NUM_CHILDREN 40
 
+int *cur_queue = {0};
+int *first_queue = {0};
+int *second_queeu = {0};
+int *third_queue = {0};
+int *forth_queue = {0};
+
+void changeQueue(int queueNumber)
+{
+    switch(queueNumber)
+    {
+        case 1:
+            cur_queue = first_queue;
+            break;
+
+        case 2:
+            cur_queue = second_queue;
+            break;
+
+        case 3:
+            cur_queue = third_queue;
+            break;
+
+        case 4:
+            cur_queue = forth_queue;
+            break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int *first_queue = {0};
-    int *second_queeu = {0};
-    int *third_queue = {0};
-    int *forth_queue = {0};
 
     int result = changePolicy();
-    if (result == 0)
+    if (result == )
     {
         printf(1, "Policy changed successfully!\n");
     }
@@ -21,10 +45,6 @@ int main(int argc, char *argv[])
     {
         printf(1, "Policy change failed!\n");
     }
-
-    int turnaroundsSum[6] = {0}; // Sum of turnaround times for each priority class
-    int waitingsSum[6] = {0};    // Sum of waiting times for each priority class
-    int CBTsSum[6] = {0};        // Sum of CBTs for each priority class
 
     int original_pid = getpid();
     int child_num = -1;
@@ -34,6 +54,15 @@ int main(int argc, char *argv[])
     {
         if (fork() == 0) // Child
         {
+            if (i == 0)
+                changeQueue(1);
+            else if (i == 10)
+                changeQueue(2);
+            else if (i == 20)
+                changeQueue(3);
+            else if (i == 30)
+                changeQueue(4); 
+
             child_num = i + 1;
             // priority = 6 - (i / 5);
             priority = (i / 5) + 1;
